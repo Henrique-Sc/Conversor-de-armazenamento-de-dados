625d9b6ab89363d4f7563f85b70cb0808b2943cf
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-03-24 10:53:05 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-print('---- Conversor de unidades de armaenaamento ----')
-
-print('''
+def intro():
+    while True:
+        print('''
 Deseja converter quais unidades?
 
 1 - Terabyte
 2 - Gibabyte
 3 - Megabyte
 4 - Kilobyte''')
+        
+        esc = int(input('\nDigite aqui: ').strip())
+        
+    
+print('---- Conversor de unidades de armazenamento ----')
 
-while True:
-    esc = int(input('\nDigite aqui: '))
+intro()

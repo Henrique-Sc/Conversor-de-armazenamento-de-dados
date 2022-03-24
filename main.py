e06e030f50c5d658c763ca2d968561218e839679
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-03-24 11:32:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+def typevalue(num):
+    if num == 1:
+        tipo = 'TB'
+    elif num == 2:
+        tipo = 'GB'
+    elif num == 3:
+        tipo = 'MB'
+    elif num == 4:
+        tipo = 'KB'
+    
+    return tipo
+        
+
 print('---- Conversor de unidades de armazenamento ----')
 
 print('''
@@ -6,6 +19,15 @@ Deseja converter quais unidades?
 1 - Terabyte
 2 - Gibabyte
 3 - Megabyte
-4 - Kilobyte''')
-        
-esc = int(input('\nDigite aqui: ').strip())
+4 - Kilobyte
+''')
+
+while True:
+    esc = int(input('Digite aqui: ').strip())
+    if esc not in (1, 2, 3, 4):
+        print('\nValor inválido! Insira um valor correto')
+    else:
+        break
+
+    
+valor = float(input(f'\nDigite o valor ({typevalue(esc)}): '))

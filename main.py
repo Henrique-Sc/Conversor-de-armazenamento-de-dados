eb780b3438b597b81373688fdd60c13bf22a55ab
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-03-24 10:45:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-print('---- conversor de unidade ----')
+print('---- Conversor de unidades de armaenaamento ----')
 
 print('''
-''')
+Deseja converter quais unidades?
+
+1 - Terabyte
+2 - Gibabyte
+3 - Megabyte
+4 - Kilobyte''')
+
+esc = int(input('Digite aqui: '))

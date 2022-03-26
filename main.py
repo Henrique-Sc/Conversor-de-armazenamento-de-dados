3f212a92f6ef56d6210d1239838082c7adccaf00
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-03-26 14:40:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -14,33 +14,32 @@ def typevalue(num):
 
 def linha():
     sleep(0.5)
-    print('\n---------------------------------------')
+    print('\n---------------------------------------\n')
     sleep(0.5)
 
 
-print('---- Conversor de unidades de armazenamento ----')
+print('---- Conversor de unidades de armazenamento ----\n')
 
 while True:
-    print('''
-Deseja converter quais unidades?
+    print('''Deseja converter quais unidades?
 
 1 - Terabyte
 2 - Gibabyte
 3 - Megabyte
 4 - Kilobyte
 ''')
+    
     esc = int(input('Digite aqui: ').strip())
     if esc not in (1, 2, 3, 4):
         print('\nValor inválido! Insira um valor correto')
-        sleep(0.5)
-        print('\n---------------------------------------')
-        sleep(0.5)
+        linha()
     else:
-        sleep(0.5)
-        print('\n---------------------------------------')
-        sleep(0.5)
+        linha()
         break
 
 tipo_valor = typevalue(esc)
-valor = float(input(f'\nDigite o valor ({tipo_valor}): '))
+valor = float(input(f'Digite o valor ({tipo_valor}): '))
 
+linha()
+
+print('Olá mundo!')

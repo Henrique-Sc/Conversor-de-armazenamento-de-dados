a80a781cc08f793af866c535170675fcaf40223c
Henrique da Silva Costa <henriquesc1442@gmail.com>
2022-03-28 20:04:35 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,20 @@ def linha():
     print('\n---------------------------------------\n')
     sleep(0.5)
 
+
 def GB_TB(gb):
     tb = gb / 1024
     return tb
+
+
+def GB_MB(gb):
+    mb = gb * 1024
+    return mb
+
+
+def GB_KB(gb):
+    kb = gb * 1024 ** 2
+    return kb
 
 
 print('---- Conversor de unidades de armazenamento ----\n')
@@ -21,4 +32,7 @@ print(f'GB para TB: {GB_TB(valor)}')
 linha()
 print(f'GB para MB: {GB_MB(valor)}')
 linha()
-print
+print(f'GB para KB: {GB_KB(valor)}')
+linha()
+
+print('<< Obrigado por testar :) >>')
